b8e3ca5553f63c491a8c8de3fb3fa060d09fbd90
Cristales <mariocardona0501@gmail.com>
2023-09-01 10:02:33 -0600

    Login & Dashboard Tests

--- src/app/components/Dashboard/dashboard/dashboard.component.spec.ts
+++ src/app/components/Dashboard/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
-import { DashboardComponent } from './dashboard.component';
+// Define a fake router service
+class FakeRouter {
+  navigateByUrl(url: string): Promise<boolean> {
+    // You can add your own logic here if needed
+    return Promise.resolve(true);
+  }
+}
+
+@Component({
+  template: `
+    <button class="agrcita" (click)="navigateToAgendarCita()">Agendar Citas</button>
+    <button class="lstcitas" (click)="navigateToListadoCitas()">Ver Citas</button>
+  `,
+})
+class TestComponent {
+  constructor(private router: Router) {}
+
+  navigateToAgendarCita() {
+    this.router.navigateByUrl('/agregar-cita');
+  }
+  navigateToListadoCitas() {
+    this.router.navigateByUrl('/listado-citas');
+  }
+}
 
 describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+  let fixture: ComponentFixture<TestComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+      providers: [{ provide: Router, useClass: FakeRouter }], // Use the fake router
+    });
 
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(TestComponent);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should navigate to "/agregar-cita" when the "Agendar Citas" button is clicked', () => {
+    const navigateSpy = spyOn(fixture.debugElement.injector.get(Router), 'navigateByUrl');
+
+    const agendarCitasButton = fixture.debugElement.query(By.css('.agrcita'));
+
+    // Trigger a click event on the "Agendar Citas" button
+    agendarCitasButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    // Check if the router's navigateByUrl method was called with the expected route
+    expect(navigateSpy).toHaveBeenCalledWith('/agregar-cita');
+  });
+
+  it('should navigate to "/listado-citas" when the "Ver Citas" button is clicked', () => {
+    const navigateSpy = spyOn(fixture.debugElement.injector.get(Router), 'navigateByUrl');
+
+    const agendarCitasButton = fixture.debugElement.query(By.css('.lstcitas'));
+
+    // Trigger a click event on the "Agendar Citas" button
+    agendarCitasButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    // Check if the router's navigateByUrl method was called with the expected route
+    expect(navigateSpy).toHaveBeenCalledWith('/listado-citas');
   });
 });
--- src/app/components/session/login/login.component.spec.ts
+++ src/app/components/session/login/login.component.spec.ts
@@ -1,6 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,16 +15,58 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have input fields and submit button', () => {
+    const emailInput = fixture.debugElement.query(By.css('input[formControlName="email"]'));
+    const passwordInput = fixture.debugElement.query(By.css('input[formControlName="password"]'));
+    const submitButton = fixture.debugElement.query(By.css('button[type="submit"]'));
+
+    expect(emailInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+    expect(submitButton).toBeTruthy();
+    expect(submitButton.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should call onSubmit when form is submitted with valid data', fakeAsync(() => {
+    const emailInput = fixture.debugElement.query(By.css('input[formControlName="email"]'));
+    const passwordInput = fixture.debugElement.query(By.css('input[formControlName="password"]'));
+
+    // Simulate user input
+    emailInput.nativeElement.value = 'test@example.com';
+    emailInput.nativeElement.dispatchEvent(new Event('input'));
+    passwordInput.nativeElement.value = 'password123';
+    passwordInput.nativeElement.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    tick();
+
+    spyOn(component, 'onSubmit');
+    const form = fixture.debugElement.query(By.css('form'));
+    form.triggerEventHandler('submit', null);
+    tick();
+
+    expect(component.onSubmit).toHaveBeenCalled();
+  }));
 });
